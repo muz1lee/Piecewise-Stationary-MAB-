366fd2e42181a5b6a39ea8a993ba36844eaec1de
muz1lee <49634740+muz1lee@users.noreply.github.com>
2021-12-21 20:38:07 +0800

    Update epsilon_greedy.py

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -3,35 +3,21 @@ import numpy as np
 
 def epsilon_greedy(arms, epsilon,ratio_greedy):
 
-    """
-    Epsilon greedy算法分配流量和选臂的具体过程
-    函数 greedy_alg将会调用函数epsilon_greedy
-    
-    input:
-    arms- 实验中的臂
-    epsilon-超参，在这里我设置的0.01
-    ratio_greedy -流量占比
-
-    output:
-    choice- 根据当前这一轮选择下一轮pull的臂
-    new_ratio- 调整新的流量
-    """
     n_arms = len(arms)
-    last_ratio = ratio_greedy[-1] # 调用上一轮的流量分配结果，意在此基础上进行调整
-    estimated_means =[arms[i].get('findex') for i in range(len(arms))]  #调用三个臂的均值
-    best_mean = np.argmax(estimated_means) #在这三个臂中找到最好的
+    last_ratio = ratio_greedy[-1] 
+    estimated_means =[arms[i].get('findex') for i in range(len(arms))] 
+    best_mean = np.argmax(estimated_means) 
     
-    be_exporatory = np.random.rand() < epsilon  # should we explore?
-    if be_exporatory:  # totally random, excluding the best_mean
+    be_exporatory = np.random.rand() < epsilon 
+    if be_exporatory:  
         other_choice = np.random.randint(0, len(estimated_means))
-        while other_choice == best_mean: # explore， 选择非best的臂
+        while other_choice == best_mean: 
             other_choice = np.random.randint(0, len(estimated_means))
         choice = other_choice
-    else:  # 选择best arm
+    else: 
         choice = best_mean 
     new_ratio=[[] for _ in range (n_arms)]
 
-    #这里是设定的流量分配方法，非choice的臂各自减少5%的流量，choice的臂则增加流量 new ratio= (95 % *last ratio of choice arm + 5% )
     for i in range(n_arms):
         if i == choice:
             new_ratio[i] = 1-0.95*(sum(last_ratio)-last_ratio[i])
