0e3a4e13b054fa28d5cb87b33ea105103d09390c
muz1lee <49634740+muz1lee@users.noreply.github.com>
2021-12-21 20:25:02 +0800

    Update Base_Algorithms.py

--- Base_Algorithms.py
+++ Base_Algorithms.py
@@ -9,18 +9,12 @@ from scipy.stats import norminvgauss
 import pandas as pd
 
 def Gaussian(mean,number):
-    """
-    根据均值生成数据
-    mean - 模拟的臂的均值
-    number - 生成数据的数量
-    """
-    return list(np.random.normal(mean,abs(mean*(1-mean)),number)) # 方差采用p*(1-p)进行计算
+    return list(np.random.normal(mean,abs(mean*(1-mean)),number))
+
 def generate_arms(i,change_points,iterations,total_backet,traffic_ratio,total_set,n_arms):
 
-    # 该轮总曝光数*每一个arm的流量占比 = 每一个arm的流量人数
     DgreedyN_list = [int(total_backet*traffic_ratio[-1][i]) for i in range(n_arms)]
     
-    # 根据不同的阶段生成不同的均值数据
     if i<=change_points[0]*iterations:
         Gaussianlist =[Gaussian(mean_matrix[0][j],DgreedyN_list[j]) for j in range(len(DgreedyN_list))]
 
@@ -33,13 +27,11 @@ def generate_arms(i,change_points,iterations,total_backet,traffic_ratio,total_se
     for i in range(n_arms): # number of arms
         total_set[i].extend(item for item in Gaussianlist[i] )
     
-    #构建Discounted greedy算法下arm的list
     arms=[]
     temp={}
     for i in range(len(total_set)):
         temp = {
-            # "previous_findex":previous_DTS_mean[i],
-            "deno":len(total_set[i]), #这个是累计的曝光数量
+            "deno":len(total_set[i]), 
             "findex":np.array(total_set[i]).mean(),
             "fvar":np.array(total_set[i]).var()
                 }
@@ -82,17 +74,13 @@ def generate_discounted_ratio_list(arms,xbar_list,n_list,decay):
         estimated_means = xbar_list
     else:
         for i in range(len(arms)):
-            # 数据处理，得到每一轮的均值和每一轮的曝光，然后存储在xbar_lisr和n_list
             xbar_list[i].append(  (arms[i].get('findex')*arms[i].get('deno') - sum([  xbar_list[i][j]*n_list[i][j] for j in range(len(n_list[i]))  ]))/(arms[i].get('deno')-sum(n_list[i]))     )
             n_list[i].append( arms[i].get('deno')-sum(n_list[i]) )
-
-        #  得到discounted rate 的list        
+      
         decay_list=[[xbar_list[i][j]*decay**(len(xbar_list[i])-1-j) for j in range(len(xbar_list[i]))] for i in range(len(arms))]
 
-        #  得到discounted rate * 曝光数的list
         decay_number_list=[[n_list[i][j]*decay**(len(n_list[i])-1-j) for j in range(len(n_list[i]))] for i in range(len(arms))]
 
-        # 通过公式对每一个臂的均值进行再计算
         estimated_means = [ sum([decay_list[i][j]*decay_number_list[i][j]  for j in range(len(decay_list[i]))])/sum(decay_number_list[i])  for i in range(len (arms) )]
 
     return estimated_means 
